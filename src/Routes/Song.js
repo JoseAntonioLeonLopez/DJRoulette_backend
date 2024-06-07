dd58387da5c308296f956196d46acef44f060995
Jose Antonio Leon Lopez <joseantonioleonlopez206@gmail.com>
2024-06-07 11:42:51 +0200

    Endpoint para borrar

--- src/Routes/Song.js
+++ src/Routes/Song.js
@@ -12,22 +12,6 @@ router.get('/songs', async (req, res) => {
   }
 });
 
-// Agregar una nueva canción
-router.post('/song', async (req, res) => {
-  const song = new Song({
-    title: req.body.title,
-    artist: req.body.artist,
-    image: req.body.image
-  });
-
-  try {
-    const newSong = await song.save();
-    res.status(201).json(newSong);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
 // Agregar nuevas canciones
 router.post('/songs', async (req, res) => {
   const songs = req.body; // Asumiendo que req.body es un array de canciones
@@ -37,6 +21,30 @@ router.post('/songs', async (req, res) => {
     res.status(201).json(newSongs);
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+});
+
+// Borrar todas las canciones
+router.delete('/songs', async (req, res) => {
+  try {
+    const result = await Song.deleteMany(); // Eliminar todas las canciones
+    res.json({ message: 'Todas las canciones han sido eliminadas', result });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Borrar una canción específica por título y artista
+router.delete('/songs/:title/:artist', async (req, res) => {
+  try {
+    const { title, artist } = req.params;
+    const result = await Song.deleteOne({ title, artist });
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: 'Canción no encontrada' });
+    }
+    res.json({ message: 'Canción eliminada exitosamente', result });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
