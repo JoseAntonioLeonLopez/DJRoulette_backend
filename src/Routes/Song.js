4badab280da3dc08d1b8b64440b9fd8d037d5de7
Jose Antonio Leon Lopez <joseantonioleonlopez206@gmail.com>
2024-06-07 11:40:09 +0200

    Endpoint para añadir varias canciones

--- src/Routes/Song.js
+++ src/Routes/Song.js
@@ -1,4 +1,3 @@
-// routes/Song.js
 const express = require('express');
 const router = express.Router();
 const Song = require('../models/Song');
@@ -14,7 +13,7 @@ router.get('/songs', async (req, res) => {
 });
 
 // Agregar una nueva canción
-router.post('/songs', async (req, res) => {
+router.post('/song', async (req, res) => {
   const song = new Song({
     title: req.body.title,
     artist: req.body.artist,
@@ -24,6 +23,18 @@ router.post('/songs', async (req, res) => {
   try {
     const newSong = await song.save();
     res.status(201).json(newSong);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Agregar nuevas canciones
+router.post('/songs', async (req, res) => {
+  const songs = req.body; // Asumiendo que req.body es un array de canciones
+
+  try {
+    const newSongs = await Song.insertMany(songs);
+    res.status(201).json(newSongs);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
